eaf22350ea56256e7a1c22468be3c6febb74e1bd
shrobon <shrobon.biswas@gmail.com>
2017-02-11 17:28:29 -0700

    Cropping done. Bad Red Boundaries

--- code/shrobonutils.py
+++ code/shrobonutils.py
@@ -1,6 +1,15 @@
 import cv2
 import numpy as np
+
+
 def find_crucial_contours(img,contour):
+	##################################################
+	# This function helps to remove the false contours
+	# and returns only the contours needed by our application
+	# Change the threshold value, and ratio of the image area (0.8 in my case )
+	# and size of the contour you are looking at (above 10000 in my case)
+	# as per your application
+	##################################################
 	arealist=[]
 	accepted_contours=[]
 	image_area = img.shape[0] * img.shape[1]
@@ -11,6 +20,10 @@ def find_crucial_contours(img,contour):
 			accepted_contours.append(i)
 
 	return accepted_contours
+
+
+
+
 
 def make_binary(thresh):
 	for i in range(0,thresh.shape[0]):
@@ -24,6 +37,9 @@ def make_binary(thresh):
 
 
 def perform_masking(img,thresh):
+	# Performs masking of 2 images using bitwise_and 
+	# to extract the region of interst
+	
 	result_b = cv2.bitwise_and(img[:,:,0],img[:,:,0],mask=thresh)
 	result_g = cv2.bitwise_and(img[:,:,1],img[:,:,1],mask=thresh)
 	result_r = cv2.bitwise_and(img[:,:,2],img[:,:,2],mask=thresh)
@@ -32,5 +48,23 @@ def perform_masking(img,thresh):
 	Final_Result[:,:,0] = result_b
 	Final_Result[:,:,1] = result_g
 	Final_Result[:,:,2] = result_r
+	'''
+	thresh1 = cv2.bitwise_not(thresh)
+	result_b1 = cv2.bitwise_or(Final_Result[:,:,0],Final_Result[:,:,0],mask=thresh1)
+	result_g1 = cv2.bitwise_or(Final_Result[:,:,0],Final_Result[:,:,0],mask=thresh1)
+	result_r1 = cv2.bitwise_or(Final_Result[:,:,0],Final_Result[:,:,0],mask=thresh1)
+
+	Final_Result[:,:,0] = result_b1
+	Final_Result[:,:,1] = result_g1
+	Final_Result[:,:,2] = result_r1
+	'''
 
 	return Final_Result
+
+def preprocess(image):
+	for i in range(0,image.shape[0]):
+		for j in range(0,image.shape[1]):
+			if image[i][j][:3] < 1:
+				image[i][j][:3] = 255
+
+	return image
--- code/start.py
+++ code/start.py
@@ -1,21 +1,22 @@
 #__author__ : Shrobon Biswas
 '''__Description__ : 
-1. To detect correct number of contours
-2. Performing masking to segment out the image 
-
 '''
 import numpy as np 
 import cv2
 from matplotlib import pyplot as plt 
-from shrobonutils import find_crucial_contours, make_binary, perform_masking
+from shrobonutils import find_crucial_contours, make_binary, perform_masking,preprocess
 
 img = cv2.imread('1.jpg')
 #cv2.imshow("Original Image",img)
 
+
+
 imgray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 blurring = cv2.GaussianBlur(imgray,(7,7),0)
 ret,thresh = cv2.threshold(blurring,0,255,cv2.THRESH_BINARY | cv2.THRESH_OTSU)
-(image, contour , _) =  cv2.findContours(thresh,cv2.RETR_LIST,cv2.CHAIN_APPROX_SIMPLE)
+(image, contour , _) =  cv2.findContours(thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+
+
 
 
 accepted_contours= find_crucial_contours(img,contour)
@@ -23,24 +24,48 @@ print "Number of herbs detected in image"
 print len(accepted_contours)
 
 
-
 thresh = make_binary(thresh)
 
 
+'''
 for i in range(0,len(accepted_contours)):
 	cv2.drawContours(thresh, accepted_contours, i, (255,255,255),thickness = -1)
-
+	
 	rect = cv2.minAreaRect(accepted_contours[i])
 	box = cv2.boxPoints(rect)
 	box = np.int0(box)
 	im = cv2.drawContours(thresh,[box],0,(255,255,255),1)
+	
+	# Strong the bounding box for later use in cropping
+	# print box
 
-	#Strong the bounding box for later use in cropping
-	print rect
+cv2.imshow("This is it !! ",thresh)
+cv2.waitKey(0)
+'''
+
+
+for i in range(0,len(accepted_contours)):
+	cv2.drawContours(thresh, accepted_contours, i, (255,255,255),thickness = -1)
+	x,y,w,h = cv2.boundingRect(accepted_contours[i])
+	
+	#cropping and writing the image 
+	mask = thresh[y:y+h,x:x+w]
+	cropped_img= img[y:y+h,x:x+w]
+	Masked = perform_masking(cropped_img,mask)
+	#Masked = preprocess(Masked)
+	cv2.imwrite(str(i)+'1.jpg',Masked)
+
 cv2.imshow('Segmented Image',thresh)
 
 
-Masked = perform_masking(img,thresh)
-cv2.imshow("After masking",Masked)
-cv2.waitKey(0)
 
+
+
+'''
+
+Masked = perform_masking(img,thresh)
+
+cv2.imshow("After masking",Masked)
+cv2.imwrite('test.jpg',Masked)
+cv2.waitKey(0)
+'''
