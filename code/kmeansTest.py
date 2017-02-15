3d12772ba4b8e0f3a744c410d97f87e782152a8b
shrobon <shrobon.biswas@gmail.com>
2017-02-14 18:23:05 -0700

    K means works excellent for segmentation git add .

---
+++ code/kmeansTest.py
@@ -0,0 +1,41 @@
+import cv2
+import numpy as np
+ 
+class Segment:
+    def __init__(self,segments=2):
+        #define number of segments, with default 5
+        self.segments= segments
+
+    def kmeans(self,image):
+        #Preprocessing step
+        image=cv2.GaussianBlur(image,(7,7),0)
+        vectorized=image.reshape(-1,3)
+        vectorized=np.float32(vectorized)
+        criteria=(cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER,10, 1.0)
+        ret,label,center=cv2.kmeans(vectorized,self.segments,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+        res = center[label.flatten()]
+    	segmented_image = res.reshape((image.shape))
+        return label.reshape((image.shape[0],image.shape[1])),segmented_image.astype(np.uint8)
+
+
+
+if __name__== "__main__":
+    import argparse
+    import sys
+
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-i", "--image", required = True,help = "Path to the image")
+    ap.add_argument("-n", "--segments", required = False,type = int,  help = "# of clusters")
+    args = vars(ap.parse_args())
+    image = cv2.imread(args["image"])
+    if len(sys.argv)==3:
+        seg = Segment()
+        label,result= seg.kmeans(image)
+
+    else:
+		seg = Segment(args["segments"])
+		label,result = seg.kmeans(image) 
+
+cv2.imshow("input",image)
+cv2.imshow("segmented",result)
+cv2.waitKey(0)
